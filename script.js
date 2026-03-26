28e1c8584054698b170e70effda02025e5dfa5da
Kina <gaireke@gmail.com>
2026-03-26 21:49:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -432,7 +432,7 @@ function toggleMapView(open) {
       mlo.style.color=mapLinkedOnly?'#fff':'#3B6D11';
       mlo.textContent=mapLinkedOnly?'✓ 只顯示關聯':'🔗 只顯示關聯';
     }
-    setTimeout(()=>{ const hadNodePos=Object.keys(nodePos).length>0; initNodePos(); drawMap(); if(!hadNodePos) saveData(); },80
+    setTimeout(()=>{ const hadNodePos=Object.keys(nodePos).length>0; initNodePos(); drawMap(); if(!hadNodePos) saveData(); },80);
   } else {
     closeMapPopup();
   }
